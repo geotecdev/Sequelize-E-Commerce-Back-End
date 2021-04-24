cf4f8006ee4813e90dd42b78bc940344c998133f
geotecdev <mikeruanedev@gmail.com>
2021-04-23 21:32:02 -0400

    update schema.sql, connection.js and package.json to switch db dialect to mysql

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'menardelbowweartracking3.database.windows.net',
-    dialect: 'mssql', //postgres'|'sqlite'|''|'mysql',
-    dialectOptions: {
-        options: { encrypt: true }
-    }
-});
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
